8eecb770e7d6fa4c345ae7808f43b8e27949e93b
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-05-26 18:47:48 +0300

    del

--- src/redux/products/selectors.js
+++
@@ -1,4 +0,0 @@
-//selectors
-export const selectProductsList = state => state.products.list;
-export const selectProductsLoading = state => state.products.loading;
-export const selectProductsError = state => state.products.error;
--- src/utils/notify.js
+++
@@ -1,9 +0,0 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-//message Toast
-export const succesNotify = () => {
-  toast.success('The product has been added to the cart!', {
-    position: toast.POSITION.TOP_LEFT,
-  });
-};
