5fa0308b6055a3a384c39837748ee99475f871ff
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2023-05-26 18:28:01 +0300

    del

--- src/components/Notification/Notification.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-//component notify message
-export default function Notification() {
-  return <ToastContainer />;
-}
--- src/utils/countProducts.js
+++
@@ -1,7 +0,0 @@
-//func calculation of all products in the cart
-export function countAllCart(products) {
-  const countProducts = products.reduce((accumulator, product) => {
-    return accumulator + product.quantity;
-  }, 0);
-  return countProducts;
-}
